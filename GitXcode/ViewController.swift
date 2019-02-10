6a0557dde78359986998c21ef28c979237e5b2f5
Александр <aleksandr@MacBook-Pro-Aleksandr-2.local>
2019-02-10 12:09:33 +0100

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Александр on 10/02/2019.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
